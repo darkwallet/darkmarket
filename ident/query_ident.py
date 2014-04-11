1d8e42bc01f8ee0e4b1a0314914811cb3d8bc69f
Amir Taaki <genjix@riseup.net>
2014-04-11 02:20:45 -0400

    added another identity

--- ident/query_ident.py
+++ ident/query_ident.py
@@ -2,7 +2,7 @@ import zmq
 c = zmq.Context()
 s = c.socket(zmq.REQ)
 s.connect("tcp://localhost:5558")
-s.send("s_tec")
+s.send("genjix")
 key = s.recv()
 if key == "__NONE__":
     print "Invalid nick"
--- ident/send_ident.py
+++ ident/send_ident.py
@@ -6,12 +6,12 @@ s.connect("tcp://localhost:5557")
 #s.send("novaprospekt", flags=zmq.SNDMORE)
 #s.send("03f881d01573c94ce7a92132479c612448140330aadbfad447543319d95af4ccc7".decode("hex"))
 # secret = b52bafc3498e5b8b594b9737ec74e4bec181c8422cb4cb6b7de81da2a9463a2a
-s.send("s_tec", flags=zmq.SNDMORE)
-s.send("02f5127844ffad50431ff9b9866c7506d7b2128e136207eb4efb89ce23ad01c0d4".decode("hex"))
+#s.send("s_tec", flags=zmq.SNDMORE)
+#s.send("02f5127844ffad50431ff9b9866c7506d7b2128e136207eb4efb89ce23ad01c0d4".decode("hex"))
 ## secret = 393baa997f92684ab6edde18af59f99e0d0cda07b09e6a4390aa09bb50fe7eb8
 #s.send("genjix", flags=zmq.SNDMORE)
 #s.send("03dfef47115e6d43d45eddbc9ee4491cc176b4821fcf9cb5c99bcd7bef0bfbea24".decode("hex"))
 ## secret = 4196a69555f599b75744029867f39a36258090f79dc616541e174d3f72ff50c2
-#s.send("caedes", flags=zmq.SNDMORE)
-#s.send("03960478444a2c18b0cd82b461c2654fc3b96dd117ddd524558268be3044192b97".decode("hex"))
+s.send("caedes", flags=zmq.SNDMORE)
+s.send("03960478444a2c18b0cd82b461c2654fc3b96dd117ddd524558268be3044192b97".decode("hex"))
 
